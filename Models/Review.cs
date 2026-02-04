67a481d3cfef541db3e18395a972e2bd919ef2b4
Rhia <rhiafhunte@gmail.com>
2026-02-04 19:32:45 +0000

    Add automatic review date handling and fix PostgreSQL timestamp issues

--- Models/Review.cs
+++ Models/Review.cs
@@ -9,12 +9,16 @@ public class Review
         public string? ReviewText {get; set;}
         public int? ReviewScore {get; set;}
         public int SpaceId { get; set;}
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
 
         public Review(int id, string reviewtext, int reviewscore)
         {
             this.Id = id;
-            this.ReviewText = reviewtext;
             this.ReviewScore = reviewscore;
+            this.ReviewText = reviewtext;
+
         }
 
         public Review() {}
